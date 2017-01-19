03c7b2e6877b90911f946f7f9373b08b1f689e73
byronbenjamin <byrongage@yahoo.com>
2017-01-19 17:13:13 -0600

    complete weather project

---
+++ src/api.js
@@ -0,0 +1,25 @@
+var _ = require('lodash');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=ce48bfeaf5328321ed51ef947d324342'
+
+var kelvinToF = function(kelvin) {
+  return Math.round((kelvin - 273.15) * 1.8 + 32) + '˚F'
+};
+
+module.exports = function(latitude,longitude) {
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+  return fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      return  {
+        city: json.name,
+        temperature: kelvinToF(json.main.temp),
+        description: _.capitalize(json.weather[0].description)
+      }
+    })
+    .catch(function(error){
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
